3af7dd17ea3e4552eea43b1ccf3e481e0cb16f8f
troykyo <git@troykyo.com>
2015-01-24 18:20:42 +0100

    restore

---
+++ Projects/main.py
@@ -0,0 +1,13 @@
+import sys
+from PySide import QtGui
+
+
+app = QtGui.QApplication(sys.argv)
+
+hello = QtGui.QPushButton("Hello world!")
+hello.resize(100, 30)
+
+hello.show()
+
+sys.exit(app.exec_())
+
